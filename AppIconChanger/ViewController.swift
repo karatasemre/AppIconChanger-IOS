40d00eb8dfa316938d175d33691f6700fdfc431f
02485400 <emre.karatas@kocholding.onmicrosoft.com>
2023-11-02 15:21:17 +0300

    fixed

--- AppIconChanger/ViewController.swift
+++ AppIconChanger/ViewController.swift
@@ -8,12 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var birthdayBtn: UIButton!
+    @IBOutlet weak var holidayBtn: UIButton!
+    @IBOutlet weak var newyearBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        birthdayBtn.addTarget(self, action: #selector(didTapAppIcon(_:)), for: .touchUpInside)
+        holidayBtn.addTarget(self, action: #selector(didTapAppIcon(_:)), for: .touchUpInside)
+        newyearBtn.addTarget(self, action: #selector(didTapAppIcon(_:)), for: .touchUpInside)
+        
     }
 
-
+   
+    @objc private func didTapAppIcon(_ sender: UIButton){
+        if(sender.tag == 1){
+            UIApplication.shared.setAlternateIconName("holiday"){ error in
+                guard error == nil else{
+                    print("Something went wrong1")
+                    return
+                }
+                print("icon updated")
+                
+            }
+        }
+        else if(sender.tag == 2){
+            UIApplication.shared.setAlternateIconName("birthday"){ error in
+                guard error == nil else{
+                    print("Something went wrong2")
+                    return
+                }
+                print("icon updated")
+                
+            }
+        }
+        else if(sender.tag == 3){
+            UIApplication.shared.setAlternateIconName("newyear"){ error in
+                guard error == nil else{
+                    print("Something went wrong3")
+                    return
+                }
+                print("icon updated")
+                
+            }
+        }
+    }
 }
 
